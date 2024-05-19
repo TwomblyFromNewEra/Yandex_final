8b4a53afca81ec41b032e752f03faec6df5f2b4f
TwomblyFromNewEra <arsenylomaka@gmail.com>
2024-05-20 01:14:00 +0300

    коллекции для mongodb

---
+++ config.js
@@ -0,0 +1,11 @@
+const SECRET_KEY = 'lalala';
+const JWT_EXPIRES_IN = 3600;
+const PORT = 3001;
+const CORS =  ["http://localhost:3000", "http://localhost:3001"];
+
+module.exports = {
+  SECRET_KEY,
+  JWT_EXPIRES_IN,
+  PORT,
+  CORS,
+};
