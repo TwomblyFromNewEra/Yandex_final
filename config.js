553100656b86a659d40ba965022491406fe28134
TwomblyFromNewEra <arsenylomaka@gmail.com>
2024-05-20 20:45:56 +0300

    Очепятка

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const SECRET_KEY = 'lalala';
 const JWT_EXPIRES_IN = 3600;
 const PORT = 3001;
-const CORS =  ["hhtps://lainv3frontend.nomoredomainswork.ru"];
+const CORS =  ["https://lainv3frontend.nomoredomainswork.ru"];
 
 module.exports = {
   SECRET_KEY,
