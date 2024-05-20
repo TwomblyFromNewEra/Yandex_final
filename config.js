81ce098de0e2a05f326e9069dded2c09e6e0cb23
TwomblyFromNewEra <arsenylomaka@gmail.com>
2024-05-20 20:38:46 +0300

    Changed BASE_URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const SECRET_KEY = 'lalala';
 const JWT_EXPIRES_IN = 3600;
 const PORT = 3001;
-const CORS =  ["http://localhost:3000", "http://localhost:3001"];
+const CORS =  ["hhtps://lainv3frontend.nomoredomainswork.ru"];
 
 module.exports = {
   SECRET_KEY,
