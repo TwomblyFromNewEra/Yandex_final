cf38e0f24ca9fff0c9376200f18358b2d4500725
TwomblyFromNewEra <113703619+TwomblyFromNewEra@users.noreply.github.com>
2024-05-23 00:37:14 +0300

    Update games.js

--- routes/games.js
+++ routes/games.js
@@ -4,47 +4,48 @@ const { checkAuth } = require('../middlewares/auth.js');
 const { checkIsVoteRequest } = require('../middlewares/games.js');
 
 const {
-  createGame,
-  findAllGames,
-  findGameById,
-  updateGame,
-  deleteGame,
-  checkEmptyFields,
-  checkIfUsersAreSafe,
-  checkIfCategoriesAvaliable,
-  checkIsGameExists,
+  createGame,
+  findAllGames,
+  findGameById,
+  updateGame,
+  deleteGame,
+  checkEmptyFields,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkIsGameExists,
 } = require('../middlewares/games');
 const {
-  sendGameCreated,
-  sendAllGames,
-  sendGameById,
-  sendGameUpdated,
-  sendGameDeleted,
+  sendGameCreated,
+  sendAllGames,
+  sendGameById,
+  sendGameUpdated,
+  sendGameDeleted,
 } = require('../controllers/games');
 
 gamesRouter.post(
-  '/games',
-  checkAuth,
-  checkEmptyFields,
-  checkIfCategoriesAvaliable,
-  findAllGames,
-  checkIsGameExists,
-  createGame,
-  sendGameCreated,
+  '/games',
+  checkAuth,
+  checkEmptyFields,
+  checkIfCategoriesAvaliable,
+  findAllGames,
+  checkIsGameExists,
+  createGame,
+  sendGameCreated,
 );
 gamesRouter.get('/games', findAllGames, sendAllGames);
 gamesRouter.get('/games/:id', findGameById, sendGameById);
 gamesRouter.put(
-    "/games/:id",
-    findGameById,
-    checkIsVoteRequest,
-    checkIfUsersAreSafe,
-    checkIfCategoriesAvaliable,
-    checkEmptyFields,
-    checkAuth,
-    updateGame,
-    sendGameUpdated
-  );
+    "/games/:id",
+    findGameById,
+    checkIsVoteRequest,
+    checkIfUsersAreSafe,
+    checkIfCategoriesAvaliable,
+    checkEmptyFields,
+    checkAuth,
+    updateGame,
+    sendGameUpdated,
+    checkIsGameExists,
+  );
 gamesRouter.delete('/games/:id', checkAuth, deleteGame, sendGameDeleted);
 
 module.exports = gamesRouter;
